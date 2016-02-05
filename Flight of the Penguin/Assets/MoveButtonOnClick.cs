5c434d78c1060a53d203faa93099022cf979d503
PetterV <petter.vilberg@gmail.com>
2016-02-05 12:26:50 +0100

    New buttons, canvas move script

---
+++ Flight of the Penguin/Assets/MoveButtonOnClick.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveButtonOnClick : MonoBehaviour {
+
+	public float posX;
+	public float frameLength = 100;
+	public float targetPosX;
+	public float moveSpeed = 10f;
+	bool moveLeft = false;
+
+	void Start (){
+		posX = transform.position.x;
+		targetPosX = posX - frameLength;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		posX = transform.position.x;
+		if (moveLeft == true) {
+			MoveButtonLeft();
+		}
+	}
+
+	void MoveButtonLeft(){
+		if (posX > targetPosX){
+			float moveTo = transform.position.x - moveSpeed;
+			transform.position = new Vector3 (moveTo, transform.position.y, transform.position.z);
+		}
+		if (posX <= targetPosX) {
+			moveLeft = false;
+		}
+	}
+
+	public void Activate (){
+		moveLeft = true;
+	}
+}
