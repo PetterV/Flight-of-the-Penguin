6cc1e1d92439468009d7243feabb5a70368d75ea
PetterV <petter.vilberg@gmail.com>
2016-02-10 18:26:41 +0100

    Event Horizon

--- Flight of the Penguin/Assets/Scripts/ObjectRotation.cs
+++ Flight of the Penguin/Assets/Scripts/ObjectRotation.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class ObjectRotation : MonoBehaviour {
 
 	// the object needs a rigidbody 2d component 
-	public int rotationSpeed = 10;
+	public float rotationSpeed = 10f;
 
 	void Update () {
-		rigidbody2D.MoveRotation(rigidbody2D.rotation + rotationSpeed * Time.deltaTime);
+		transform.Rotate(new Vector3(0,0,0 - rotationSpeed * Time.deltaTime));
 	}
 }
