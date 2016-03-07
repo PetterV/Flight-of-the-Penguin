f9a15fdde3ae2682dcbcd4c3388fd251a3b3855e
PetterV <petter.vilberg@gmail.com>
2016-03-07 21:02:32 +0100

    Red Light ferdig

--- Flight of the Penguin/Assets/Scripts/Movement.cs
+++ Flight of the Penguin/Assets/Scripts/Movement.cs
@@ -358,7 +358,7 @@ public class Movement : MonoBehaviour {
 			goalHit=true;
 			animator.SetInteger("FlySpeed", 5);
 		}
-		if (other.gameObject.tag == "Lazer" && debug == false) {
+		if (other.gameObject.tag == "Laser" && debug == false) {
 			//play explosions and 2 sec later restart? feels nice with insta restart. mby sound
 			Dead();
 			
