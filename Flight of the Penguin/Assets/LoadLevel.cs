0f6bf9b8f4d5b4638ca388d73ebf7544493b8c85
PetterV <petter.vilberg@gmail.com>
2016-04-25 22:01:41 +0200

    Level Select-knapp funker nå

---
+++ Flight of the Penguin/Assets/LoadLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+	public int levelToLoad;
+	public string sceneToLoad;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	public void LoadLevelFromInt(int levelToLoad){
+		Application.LoadLevel (levelToLoad);
+	}
+
+	public void LoadLevelFromName(string sceneToLoad){
+		Application.LoadLevel (sceneToLoad);
+	}
+}
